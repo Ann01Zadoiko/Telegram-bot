60159c186b34e1bd6501ec8e51c61f8763da393d
Anna <anna.zadoiko@gmail.com>
2024-02-29 14:22:15 +0200

    Version 1.0 - at work - list - converter - full name - properties (token, name) - reset the list

--- src/main/java/com/example/please/bot/TelegramBot.java
+++ src/main/java/com/example/please/bot/TelegramBot.java
@@ -100,7 +100,7 @@ public class TelegramBot extends TelegramLongPollingBot {
                 sendMessage(charId, "Бажаю гарного робочого дня!");
             }
 
-            if (messageText.length() < 15 && !(isACommand(messageText)) && stringBuilder.length < 2){
+            if (!(isACommand(messageText)) && stringBuilder.length < 2){
 
                 if (isCyrillic(messageText)){
                     String password = Converter.convertPassword(messageText);
