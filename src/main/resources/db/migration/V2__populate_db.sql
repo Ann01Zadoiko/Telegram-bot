20fbb344a3ddccb2f47f6513ef264fe009462ddd
Admin <anna.zadoiko@gmail.com>
2024-04-01 09:57:10 +0300

    Version (2.0): - refactoring - /help - list of sick/vacation/work - back

--- src/main/resources/db/migration/V2__populate_db.sql
+++
@@ -1,4 +0,0 @@
---insert into users (id, chat_id, full_name, at_work, status) values (1, 391736560, 'Анна Задойко', 0, 'WORK');
---
---
---insert into notifications (time_of_notification, id_user) values ('0 0 9 * * MON-FRI', 1);
